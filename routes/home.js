31b91067840d9c60b25b2e1c8e6b635b8a347ab9
fzi2000 <114130716+fzi2000@users.noreply.github.com>
2023-07-07 13:35:38 +0530

    Add files via upload

--- routes/home.js
+++ routes/home.js
@@ -3,9 +3,13 @@ const router = express.Router();
 
 router.get("/", async (req, res, next) => {
   return res.status(200).json({
-    title: "Express Testing",
-    message: "The app is working properly! Welcome to Mail-O-Matic. Please try our extension",
+    title: "\x1b[34mExpress Testing\x1b[0m", // Blue color
+    message: "\x1b[32mThe app is working properly! Welcome to Mail-O-Matic. Please try our extension\x1b[0m" // Green color
+  
+    // title: "Express Testing",
+    // message: "The app is working properly! Welcome to Mail-O-Matic. Please try our extension",
   });
+  return res.status(200).json(response);
 });
 
 module.exports = router;
